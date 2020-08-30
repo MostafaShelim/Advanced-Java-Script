cefe8dd3bbe7ca6e44bc054bbdafc1b84583e5d6
MostafaShelim <mostafarmstu@gmail.com>
2020-08-30 22:12:58 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+// let pakhi;
+// console.log(pakhi);//undefined
+
+// function add(n1, n2){
+//     console.log(n1 + n2);//undefined cause return kori nai
+//     // return likhleo kicho dei nai undefined
+// }
+// console.log(add(12, 12));
+
+function add(n1){
+    return console.log(n1 + n2);
+
+}
+add(12, 12);//parameter ekta kom dichi unde
