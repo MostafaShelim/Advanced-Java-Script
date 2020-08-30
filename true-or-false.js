3bc95fdc1df33f4826c8861034ac4c24a35bf3aa
MostafaShelim <mostafarmstu@gmail.com>
2020-08-30 21:37:02 +0600

    truthy or falsy

---
+++ true-or-false.js
@@ -0,0 +1,18 @@
+// 0 
+// empty string
+// null
+// undefined
+// NaN
+//false
+// all above are condition is false
+
+//'0'
+//' '
+//[]
+//true
+// all above are truthy
+const age = 110;
+const name = '0';
+if(name){
+    console.log("condition is true");
+}else console.log("condtion is false");
