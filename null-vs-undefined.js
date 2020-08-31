bb9ff388f99ad486a2a475ecbe346612e9a34429
MostafaShelim <mostafarmstu@gmail.com>
2020-08-31 12:48:34 +0600

    null vs undefined

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -7,8 +7,18 @@
 // }
 // console.log(add(12, 12));
 
-function add(n1){
-    return console.log(n1 + n2);
+// function add(n1, n2){
+//     return console.log(n1 + n2);
 
+// }
+// add(12);//parameter ekta kom dichi undefined
+
+const premik = {
+    name: "mostafa";
+    phone: 022
 }
-add(12, 12);//parameter ekta kom dichi unde
+console.log(premik.gf)//undefined
+const arr = [1, 3, 4];
+console.log(arr[5])//undefined
+// undefined: jeta nai seta niye kaj kora 
+// null: eta thakbe but ami etake null diye dile
