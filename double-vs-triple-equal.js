df700e66e28ac8ea476034c454fb34fb6cf0ae27
MostafaShelim <mostafarmstu@gmail.com>
2020-08-31 13:07:07 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,37 @@
+const first = 2;
+const two = "2";
+if(first == two){
+    console.log("The condition is true");
+}//it is true
+else {
+    console.log("The condition is false");
+}
+
+let val1 = 2;
+let val2 = "2";
+if(val1 === val2){
+    console.log("The condition is true");
+}//it is false
+else {
+    console.log("The condition is false");
+}
+
+val1 = false;//same to val1 = true;
+val2 = 0;//same to val2 = 1; onno kono number hole hbe na
+
+if(val1 == val2){
+    console.log("the condition is true");
+}//it is true
+else {
+    console.log("the condition is false");
+}
+
+val1 = false;//same to val1 = true;
+val2 = 0;//same to val2 = 1
+
+if(val1 === val2){
+    console.log("the condition is true");
+}//it is false;
+else {
+    console.log("the condition is false");
+}
